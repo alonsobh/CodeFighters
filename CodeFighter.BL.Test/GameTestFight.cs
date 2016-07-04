c836e0842db7bd36839147904471771423000387
Alonso Bustos_Herrera <alonsobh@gmail.com>
2016-07-04 10:53:39 -0500

    Start Game with Names

---
+++ CodeFighter.BL.Test/GameTestFight.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFighter.BL.Test
+{
+    [TestClass]
+    public class GameTestFight
+    {
+        private Game game;
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game("Juan","Jose");
+        }
+
+        [TestMethod]
+        public void GameStart()
+        {
+            Assert.AreEqual("Juan", game.NamePlayer1);
+            Assert.AreEqual("Jose", game.NamePlayer2);
+        }
+    }
+}
--- CodeFighter.BL/Game.cs
+++ CodeFighter.BL/Game.cs
@@ -2,13 +2,19 @@
 {
     public class Game
     {
+        public Game(string namePlayer1, string namePlayer2)
+        {
+            NamePlayer1 = namePlayer1;
+            NamePlayer2 = namePlayer2;
+        }
+
         //Player 1
-        public string NamePlayer1 { get; set; }
+        public string NamePlayer1 { get; private set; }
         public int LifePLayer1 { get; set; }
         public int EnergyPlayer1 { get; set; }
 
         //Player 2
-        public string NamePlayer2 { get; set; }
+        public string NamePlayer2 { get; private set; }
         public int LifePLayer2 { get; set; }
         public int EnergyPlayer2 { get; set; }
     }

ec6e26c6c2844c6e94f2bc001e7a1bbc4a4a9f68
Alonso Bustos_Herrera <alonsobh@gmail.com>
2016-07-04 12:13:08 -0500

    Refactory Player

--- CodeFighter.BL/Game.cs
+++ CodeFighter.BL/Game.cs
@@ -4,52 +4,15 @@ namespace CodeFighter.BL
 {
     public class Game
     {
-        public bool CanApplySpecial(bool isPlayer1)
-        {
-            return isPlayer1 ? EnergyPlayer1 == 100 : EnergyPlayer2 == 100;
-        }
-        public bool CanApplyHeal(bool isPlayer1)
-        {
-            return isPlayer1 ? EnergyPlayer1 == 100 : EnergyPlayer2 == 100;
-        }
-
-        //TODO:set to internal
-        public void FillEnergy(bool isPlayer1)
-        {
-            if (isPlayer1)
-                EnergyPlayer1 = 100;
-            else
-                EnergyPlayer2 = 100;
-        }
-        //TODO:set to internal
-        public void SetLife(bool isPlayer1, int value)
-        {
-            if (isPlayer1)
-                LifePlayer1 = value;
-            else
-                LifePlayer2 = value;
-        }
-
 
         public Game(string namePlayer1, GameRoleList rolePlayer1, string namePlayer2, GameRoleList rolePlayer2)
         {
-            NamePlayer1 = namePlayer1;
-            NamePlayer2 = namePlayer2;
-            RolePlayer1 = rolePlayer1;
-            RolePlayer2 = rolePlayer2;
+            Player1 = new Player(namePlayer1, rolePlayer1);
+            Player2 = new Player(namePlayer2, rolePlayer2);
         }
 
-        //Player 1
-        public string NamePlayer1 { get; private set; }
-        public int LifePlayer1 { get; set; } = 200;
-        public int EnergyPlayer1 { get; set; }
-        public GameRoleList RolePlayer1 { get; private set; }
-
-        //Player 2
-        public string NamePlayer2 { get; private set; }
-        public int LifePlayer2 { get; set; } = 200;
-        public int EnergyPlayer2 { get; set; }
-        public GameRoleList RolePlayer2 { get; private set; }
+        public Player Player1 { get; private set; }
+        public Player Player2 { get; private set; }
 
 
         public void Punch(bool isPlayer1)
@@ -74,36 +37,27 @@ namespace CodeFighter.BL
 
         private void ApplyMove(bool isPlayer1, Move move)
         {
-            if (isPlayer1)
+            var attacker = isPlayer1 ? Player1 : Player2;
+            var defendant = isPlayer1 ? Player2 : Player1;
+            if (move.RequiresFullEnergy)
             {
-                if (move.RequiresFullEnergy)
-                {
-                    if (EnergyPlayer1 == 100)
-                        EnergyPlayer1 = 0;
-                    else
-                        throw new DataException("Energy Must Be Full");
-                }
-                LifePlayer1 += move.EfectOnSelf;
-                LifePlayer2 -= move.Power;
-                EnergyPlayer1 += move.EnergyBonus;
+                if (attacker.Energy == 100)
+                    attacker.Energy = 0;
+                else
+                    throw new DataException("Energy Must Be Full");
             }
-            else
-            {
-                if (move.RequiresFullEnergy)
-                {
-                    if (EnergyPlayer2 == 100)
-                        EnergyPlayer2 = 0;
-                    else
-                        throw new DataException("Energy Must Be Full");
-                }
-                LifePlayer2 += move.EfectOnSelf;
-                LifePlayer1 -= move.Power;
-                EnergyPlayer2 += move.EnergyBonus;
-            }
-            if (LifePlayer1 > 200) LifePlayer1 = 200;
-            if (LifePlayer2 > 200) LifePlayer2 = 200;
-            if (EnergyPlayer1 > 100) EnergyPlayer1 = 100;
-            if (EnergyPlayer2 > 100) EnergyPlayer2 = 100;
+            attacker.Life += move.EfectOnSelf;
+            defendant.Life -= move.Power;
+            attacker.Energy += move.EnergyBonus;
+
+            ValidateLifeAndEnergy(Player1);
+            ValidateLifeAndEnergy(Player2);
+        }
+
+        private void ValidateLifeAndEnergy(Player player)
+        {
+            if (player.Life > 200) player.Life = 200;
+            if (player.Energy > 100) player.Energy = 100;
         }
     }
 }
---
+++ CodeFighter.BL/Player.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeFighter.BL
+{
+    public class Player
+    {
+        public Player(string name, GameRoleList role)
+        {
+            Name = name;
+            Role = role;
+        }
+
+        public string Name { get; private set; }
+        public GameRoleList Role { get; private set; }
+
+        public int Energy { get; set; } = 0;
+        public int Life { get; set; } = 200;
+
+
+        public bool CanApplySpecial()
+        {
+            return Energy == 100;
+        }
+        public bool CanApplyHeal()
+        {
+            return Energy == 100;
+        }
+
+        //TODO:set to internal
+        public void FillEnergy()
+        {
+            Energy = 100;
+        }
+        //TODO:set to internal
+        public void SetLife(int value)
+        {
+            Life = value;
+        }
+
+    }
+}

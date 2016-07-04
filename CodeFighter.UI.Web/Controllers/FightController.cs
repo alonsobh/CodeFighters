cee71cbda892fe554d7ea2b1f89f401f18fcaf00
israel.nizama <israel.nizama@INIZAMA>
2016-07-04 12:51:10 -0500

    Display initial values

--- CodeFighter.UI.Web/Controllers/FightController.cs
+++ CodeFighter.UI.Web/Controllers/FightController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeFighter.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,21 @@ namespace CodeFighter.UI.Web.Controllers
 {
     public class FightController : Controller
     {
+        private Game Game
+        {
+            get
+            {
+                return (Session["game"] as Game);
+            }
+        }
+
         // GET: Fight
         public ActionResult Index()
         {
+            ViewBag.PlayerName1 = Game.Player1.Name;
+            ViewBag.PlayerName2 = Game.Player2.Name;
+            ViewBag.PlayerScore1 = Game.Player1.Life.ToString() + "/200";
+            ViewBag.PlayerScore2 = Game.Player2.Life.ToString() + "/200";
             return View();
         }
     }
--- CodeFighter.UI.Web/Controllers/HomeController.cs
+++ CodeFighter.UI.Web/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
 ﻿using System.Web.Mvc;
+using CodeFighter.BL;
+using System;
 
 namespace CodeFighter.UI.Web.Controllers
 {
     public class HomeController : Controller
     {
+
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Go()
+        public ActionResult Go(string Player1, string Player2, string PlayerType1, string PlayerType2)
         {
+            Session["game"] = new Game(Player1, (GameRoleList) Enum.Parse(typeof(GameRoleList), PlayerType1), Player2, (GameRoleList) Enum.Parse(typeof(GameRoleList), PlayerType2));
             return RedirectToAction("Index", "Fight");
         }
     }

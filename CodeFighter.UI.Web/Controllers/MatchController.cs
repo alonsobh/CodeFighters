299cb31f87aacd70e2db56a783c6e595ff0f841f
Alonso Bustos_Herrera <alonsobh@gmail.com>
2016-07-04 12:55:58 -0500

    AddMatchController

---
+++ CodeFighter.UI.Web/Controllers/MatchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using CodeFighter.BL;
+
+namespace CodeFighter.UI.Web.Controllers
+{
+    public class MatchController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        private Game CurrentGame
+        {
+            get { return (Game)Session["CurrentGame"]; }
+            set { Session["CurrentGame"] = value; }
+        }
+    }
+}

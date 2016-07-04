c2ca0a5aa4c0c6321454c7af778d7b9a9a28732f
Alonso Bustos_Herrera <alonsobh@gmail.com>
2016-07-04 10:48:41 -0500

    Add Game Class

---
+++ CodeFighter.BL/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeFighter.BL
+{
+    public class Game
+    {
+        //Player 1
+        public string NamePlayer1 { get; set; }
+        public int LifePLayer1 { get; set; }
+        public int EnergyPlayer1 { get; set; }
+
+        //Player 2
+        public string NamePlayer2 { get; set; }
+        public int LifePLayer2 { get; set; }
+        public int EnergyPlayer2 { get; set; }
+    }
+}
